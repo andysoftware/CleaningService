7eca08d8de8fd5cf55f33626e50de9c6c249bdf1
dulguun <dulguunb@protonmail.com>
2019-03-20 23:29:06 +0100

    changing the port number because heroku requires env variable to map it port:80

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,10 @@
-const port = 3000;
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const PORT = process.env.PORT || 5000
 var app = express();
 
 app.use(logger('dev'));
@@ -15,5 +14,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public/view')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.listen(port,() => console.log(`listening on http://localhost:${port}`));
+app.listen(PORT,() => console.log(`listening on port ${PORT}`));
 module.exports = app;

f2538961ec790716b3ae60bb8a0c915f09f41546
dulguun <dulguunb@protonmail.com>
2019-03-20 22:46:32 +0100

    changing the view folder of the application

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,8 @@
+const port = 3000;
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,9 +12,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'public/view')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.listen(port,() => console.log(`listening on http://localhost:${port}`));
 module.exports = app;

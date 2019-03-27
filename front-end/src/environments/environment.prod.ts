2ae6a676e837da31f146962603b14e1b5446bf3e
dulguun <dulguunb@protonmail.com>
2019-03-27 08:20:05 +0100

    adding client secret keys for env

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA2s7NZqBdEzcD4iYf7uk3zXzasC3_XYss",
+    authDomain: "cleaning-service-e3097.firebaseapp.com",
+    databaseURL: "https://cleaning-service-e3097.firebaseio.com",
+    projectId: "cleaning-service-e3097",
+    storageBucket: "cleaning-service-e3097.appspot.com",
+    messagingSenderId: "284179261282"
+  }
 };
--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyA2s7NZqBdEzcD4iYf7uk3zXzasC3_XYss",
+    authDomain: "cleaning-service-e3097.firebaseapp.com",
+    databaseURL: "https://cleaning-service-e3097.firebaseio.com",
+    projectId: "cleaning-service-e3097",
+    storageBucket: "cleaning-service-e3097.appspot.com",
+    messagingSenderId: "284179261282"
+  }
 };
 
 /*

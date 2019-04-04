6c192b4731910406d2948d57c4af511b6af48928
Ahmed <blejahmed@gmail.com>
2019-04-04 19:52:27 +0200

    corrected User class

--- front-end/src/app/User.ts
+++ front-end/src/app/User.ts
@@ -1,7 +1,14 @@
-export interface User{
+export class User{
     id: String;
     name:String;
     avatar: String;
     role: String;
     description:String;
+	
+	constructor(user:any){
+		this.id = user.id;
+		this.name = user.name;
+		this.avatar = user.avatar;
+		this.description = user.description;
+    }
 }

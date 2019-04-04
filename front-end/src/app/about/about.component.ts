be485dcb6824ebc5623c8363af539de89aac8dfc
KimYuri94 <kimyuri31@gmail.com>
2019-04-04 20:24:50 +0200

    adding a about page component and test

---
+++ front-end/src/app/about/about.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  contactContents:any[] = [];
+  constructor() { }
+
+  ngOnInit() {
+    this.contactContents = [
+      {type:"text",placeHolder:"Service Name",requiredName:"Name"},
+      {type:"number",placeHolder:"How many times",requiredName:"Quantity"},
+      {type:"datetime-local",placeHolder:"Date and Time",requiredName:"Date"},
+      {type:"text",placeHolder:"Message \special requriements",requiredName:"Message"}
+    ]
+  }
+
+}

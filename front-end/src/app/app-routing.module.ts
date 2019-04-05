dd57176751dce7b2cdfcca9a80ba28fc2397d95d
Nazerke Seidan <naz@Nazerkes-MacBook-Pro.local>
2019-04-06 00:05:04 +0200

    added the /main route

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AboutComponent} from './about/about.component';
+import {MainComponent} from './main/main.component';
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
+  { path: 'main', component: MainComponent },
+
 ];
 
 @NgModule({
